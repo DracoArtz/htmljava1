977b7169c97bc557312c57768d002f3d0d9a8228
DracoArtz <dhuizar@email.neit.edu>
2024-11-05 17:31:04 -0500

    big game and game scenes DONE

--- Week6/js/game.js
+++ Week6/js/game.js
@@ -10,7 +10,10 @@ var ctx = c.getContext(`2d`)
 var fps = 1000/60
 var timer = setInterval(main, fps)
 
+var gameScenes = ["start", "game", "gameOver"]
+var currentScene = gameScenes[0];
 
+var krabbyPatty = document.getElementById("KrabbyPatty");
 /*------------Declare Variables Here--------*/
 
 var player = new GameObject();
@@ -19,7 +22,7 @@ player.w = 40;
 player.h = 40;
 player.friction = 0.9;
 var playerSpeed = 5;
-
+var score = 0;
 
 //generate enemies
 var enemies = [];
@@ -31,6 +34,7 @@ for(var i = 0; i<numberOfEnemies; i++){
     enemies[i].color = "red";
     enemies[i].w = 20;
     enemies[i].h = 20;
+    enemies[i].vy = 3;
     enemies[i].x = rand(0, c.width);
     enemies[i].y = rand(0, c.height);
 
@@ -44,10 +48,47 @@ This is the function that makes the game work
 ---------------------------------------------*/
 
 function main()
-{
-    //erases the screen
+{ 
+       //erases the screen
     ctx.clearRect(0,0,c.width,c.height); 
+    
+    switch(currentScene){
+        case "start":
+        console.log(currentScene);    
+        ctx.font = "40px arial";
+    ctx.fillText(`click to start. over 100 to win`, 150, c.height/2);
+    addEventListener(`click`, play);
+         break;
 
+        case "game":
+            console.log(currentScene);    
+            game();
+            if(score > 100){
+                win();
+            }
+         break;
+
+        case "gameOver":
+            console.log(currentScene);   
+            ctx.font = "40px arial";
+    ctx.fillText(`You Win!!`, c.width/2 - 100, c.height/2); 
+         break;
+
+        
+    }
+
+}
+
+function play(){
+    currentScene = gameScenes[1];
+}
+
+function win(){
+        currentScene = gameScenes[2];
+}
+
+function game(){
+    
     //Any changes to numbers
     if(a == true || left == true){
         player.vx = -playerSpeed;
@@ -72,13 +113,41 @@ function main()
 
     //draw the pictures
     for(var i = 0; i < enemies.length; i++){
-        enemies[i].render();
+        enemies[i].move();
+        //enemies[i].render();
+        enemies[i].renderImage(krabbyPatty);
+        //reset enemies off screen bottom
+        if(enemies[i].y > c.height + enemies[i].h){
+            enemies[i].y = rand(-c.height, 0);
+            enemies[i].x = rand(0, c.width);
+            if(enemies[i].vy == 3){
+                if(score > 0){
+            score --;
+                }
+            }
+
+            //enemies[i].vy = -3;
+
+        }
+        //reset off screen top
+        if(enemies[i].y < - enemies[i].h){
+            enemies[i].y = rand(-c.height, 0);
+            enemies[i].x = rand(0, c.width);
+            if(enemies[i].vy == -3){
+                score += 5;
+                enemies[i].vy = 3;
+            }
+        }
+       if(player.overlaps(enemies[i])){
+        enemies[i].vy = -3;
+       }
     }
 
     player.move();
     player.render();
+    ctx.font = "40px arial";
+    ctx.fillText(`score: ${score}`, 10, 50);
 }
-
 //random number generator
 function rand(_low, _high)
 {
